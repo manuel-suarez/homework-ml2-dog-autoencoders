6bc3deffb453a82c0030d9320451009124815596
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-30 15:43:46 -0500

    Created crop images

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ else: PATH = '/home/est_posgrado_manuel.suarez/data/all-dogs'
 IMAGES = os.listdir(PATH)
 print('There are',len(IMAGES),'images. Here are 5 example filesnames:')
 print(IMAGES[:5])
+
+os.mkdir('../tmp')
+os.mkdir('../tmp/images')
+
+# CREATE RANDOMLY CROPPED IMAGES
+for i in range(500000):
+    img = Image.open(PATH + IMAGES[i%len(IMAGES)])
+    img = img.resize(( 100,int(img.size[1]/(img.size[0]/100) )), Image.ANTIALIAS)
+    w = img.size[0]; h = img.size[1]; a=0; b=0
+    if w>64: a = np.random.randint(0,w-64)
+    if h>64: b = np.random.randint(0,h-64)
+    img = img.crop((a, b, 64+a, 64+b))
+    img.save('../tmp/images/'+str(i)+'.png','PNG')
+    if i%100000==0: print('created',i,'cropped images')
+print('created 500000 cropped images')
